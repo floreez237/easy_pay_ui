fddfa879f960aaa95ce42bf5acae035c59ca5872
floreez237 <florianlowe12@gmail.com>
2021-04-27 11:51:33 +0100

    Completed Confirmation Pane

--- src/components/ConfirmTransaction.js
+++ src/components/ConfirmTransaction.js
@@ -1,29 +1,63 @@
 import React from "react";
-import {Col, Row, Container} from 'react-bootstrap';
-import {LOGOS} from '../utils/Logos';
+import {ICONS} from '../utils/Icons';
+import '../css/bootstrap-4.4.1/scss/bootstrap.scss';
+import {Button} from "@material-ui/core";
 
-const TxItem = (props) =>{
-
+const TxItem = (props) => {
     return (
-      <div>
-          <span>
-              <img src={props.logo} alt={"logo"}/>
-              <p>{props.label}</p>
-          </span>
-          <p>{props.value}</p>
-      </div>
+        <div style={{
+            position: "absolute",
+            marginTop: "16%",
+            marginLeft: props.marginLeft,
+            width: "auto",
+            height: "auto"
+        }}>
+            <div>
+                <img src={props.logo} alt={"logo"} className={"confirm_item_logo"}/>
+                <p className={"confirm_item_label"}>{props.label}</p>
+            </div>
+
+            <br/><p className={"confirm_item_value"}>{props.value}</p>
+        </div>
     );
 }
 
-export default function ConfirmTransaction() {
+const CustomSeparation= (props)=>{
     return (
-        <Container className={"confirm_container"}>
-            <Row className={"header"}>
-                <Col><p>Confirm Transaction</p></Col>
-            </Row>
-            <Row>
-                <Col><TxItem logo={LOGOS.bill} label={"test"} value={"testValue"}/></Col>
-            </Row>
-        </Container>
+        <React.Fragment><br/>
+            <hr style={{
+                position: "relative",
+                top: props.top,
+                marginTop: "20px",
+                marginBottom: "15px",
+                marginRight: "10px",
+                marginLeft: "10px"
+            }}/>
+            <br/></React.Fragment>
+    )
+}
+
+export default function ConfirmTransaction() {
+    const firstCol = "7%";
+    const secondCol = "55%";
+    const top = "27%";
+    return (
+        <div className={"confirm_container"}>
+            <div className={"header"}>
+                <p>Confirm Transaction</p>
+            </div>
+            <TxItem logo={ICONS.service} label={"Service:"} value={"Airtime"} marginLeft={firstCol}/>
+            <CustomSeparation top={top}/>
+            <TxItem logo={ICONS.provider} label={"Source:"} value={"Orange"} marginLeft={firstCol}/>
+            <TxItem logo={ICONS.phone} label={"Phone Number:"} value={"+237 698223844"} marginLeft={secondCol}/>
+            <CustomSeparation top={top}/>
+            <TxItem logo={ICONS.provider} label={"Destination:"} value={"MTN"} marginLeft={firstCol}/>
+            <TxItem logo={ICONS.phone} label={"Phone Number:"} value={"+237 678223844"} marginLeft={secondCol}/>
+            <CustomSeparation top={top}/>
+            <TxItem logo={ICONS.amount} label={"Amount:"} value={"20.0 FCFA"} marginLeft={firstCol}/>
+            <TxItem logo={ICONS.fees} label={"Fees:"} value={"10.0 FCFA"} marginLeft={secondCol}/>
+            <Button variant={"contained"} className={"customBtn backBtn"}>Back</Button>
+            <Button variant={"contained"} className={"customBtn nextBtn"}>Next</Button>
+        </div>
     )
 }
---
+++ src/utils/Icons.js
@@ -0,0 +1,28 @@
+import accNumber from '../resources/icons/account number.png';
+import amount from '../resources/icons/Amount.png';
+import amountWhite from '../resources/icons/amount whitw.png';
+import billId from '../resources/icons/Bill iD.png';
+import dateWhite from '../resources/icons/date white.png';
+import fees from '../resources/icons/Fees.png';
+import phone from '../resources/icons/Phone.png';
+import provider from '../resources/icons/Provider.png';
+import receiptNo from '../resources/icons/Receipt No.png';
+import service from '../resources/icons/Service.png';
+import serviceWhite from '../resources/icons/service white.png';
+import website from '../resources/icons/Website.png';
+
+
+export const ICONS={
+    accountNo: accNumber,
+    amount: amount,
+    amountWhite: amountWhite,
+    billId: billId,
+    dateWhite: dateWhite,
+    fees: fees,
+    phone: phone,
+    provider: provider,
+    receiptNo: receiptNo,
+    service: service,
+    serviceWhite: serviceWhite,
+    website: website
+}
