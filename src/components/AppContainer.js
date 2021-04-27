795e6303f9b52d7cbfdf9f7e31783c451e623f7c
floreez237 <florianlowe12@gmail.com>
2021-04-27 12:29:05 +0100

    Completed The LoadingPane.js

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from "./NavBar";
 import mavianceLogo from "../resources/Maviance Logo.png";
 import "../css/AppContainer.css";
+import ConfirmTransaction from "./ConfirmTransaction";
+import TransactionPane from "./TransactionPane";
 import LoadingPane from "./LoadingPane";
 
 
--- src/components/LoadingPane.js
+++ src/components/LoadingPane.js
@@ -1,8 +1,25 @@
 import React from "react";
 import CircularProgress from '@material-ui/core/CircularProgress';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        '& > * + *': {
+            marginLeft: theme.spacing(2),
+        },
+    },
+}));
 
 export default function LoadingPane() {
     return (
-        <CircularProgress color="secondary" />
+        <div><CircularProgress style={{
+            position: "absolute",
+            top: "37.5%",
+            left: "45%",
+            color: "#60E160"
+        }} size={100}/>
+            <p className={"loading_label"}>Transaction in Progress...</p>
+        </div>
     );
 }
